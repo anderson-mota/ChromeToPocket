ad74c41f0f1ae3da900fb190f1ad2e57150f1f2e
Anderson Mota <anderson.mota12@gmail.com>
2015-02-24 21:06:22 +0000

    Metodos para salvar os Bookmarks

--- js/main.js
+++ js/main.js
@@ -3,11 +3,14 @@
  */
 var BookmarksToPocket = function($) {
 
-    var appHost = "http://chrome-bookmarks-to-pocket.herokuapp.com/";
+    var appHost = "https://chrometopocket-anderson-mota.c9.io/";
     var pocketHost = "https://getpocket.com/";
-    var pocketCustomerKey = "38238-3a41f1da78d6970fe24870f8";
-    var pocketCode, pocketAccessToken;
-    var bookmarks = {};
+    var pocketConsumerKey = "38238-3a41f1da78d6970fe24870f8";
+    var pocketCode = "ea702271-f5b5-55ef-e47c-2fffff";
+    var pocketAccessToken;
+    var bookmark = {};
+    var bookmarks = [];
+    var bookmarksAdded = [];
 
     document.getElementById("inputImporter").addEventListener("change", handleFileSelect, false);
 
@@ -32,11 +35,6 @@ var BookmarksToPocket = function($) {
         return $parent.parent('dt').makeTags(tags);
     };
 
-    function init() {
-        console.log('call> BookmarksToPocket:pocketOAuthRequest');
-        pocketOAuthRequest();
-    }
-
     function parseBookmarksToJson() {
         var source = new DOMParser().parseFromString(this.result, "text/html");
         console.log(source);
@@ -46,15 +44,21 @@ var BookmarksToPocket = function($) {
             console.log('dt>', $(this));
 
             if ($(this).find('a').length) {
-                var title = $(this).find('a').first().text();
-                var url = $(this).find('a').first().attr('href');
-                var tags = $(this).makeTags();
+                bookmark = {};
+                bookmark.title = $(this).find('a').first().text();
+                bookmark.url = $(this).find('a').first().attr('href');
+                bookmark.time = $(this).find('a').first().attr('add_date');
+                bookmark.tags = $(this).makeTags();
 
-                console.log('title>', title);
-                console.log('url>', url);
-                console.log('tags>', tags.join(', '));
+                console.log('title>', bookmark.title);
+                console.log('url>', bookmark.url);
+                console.log('tags>', bookmark.tags.join(', '));
+                
+                bookmarks.push(bookmark);
             }
         });
+        
+        saveBookmarks();
     }
 
     function handleFileSelect(e) {
@@ -64,6 +68,16 @@ var BookmarksToPocket = function($) {
         if (files) {
             reader.readAsText(files);
         }
+    }
+    
+    function saveBookmarks() {
+        pocketOAuthAuthorize().done(function(){
+            if (!pocketAccessToken) { 
+                console.error("Not defined pocketAccessToken, pocket not authorized.");
+                return;
+            }
+            $.each(bookmarks, pocketAdd);
+        });
     }
 
     function setPocketCode(data) {
@@ -76,20 +90,25 @@ var BookmarksToPocket = function($) {
         pocketAccessToken = data;
         console.log('set> pocketAccessToken', pocketAccessToken);
     }
+    
+    /**
+    /* Pocket Functions
+    */
 
-    //@TODO Access-Control-Allow-Origin need redirect_uri
+    //@TODO Access-Control-Allow-Origin, need redirect_uri and need Request by server side
     function pocketOAuthRequest() {
-        $.ajax({
+        return $.ajax({
             type: 'post',
+            contentType: 'application/json; charset=UTF-8',
             url: pocketHost + 'v3/oauth/request',
-            data: {"consumer_key": pocketCustomerKey, "redirect_uri": appHost},
+            data: {"consumer_key": pocketConsumerKey, "redirect_uri": appHost},
             success: setPocketCode
         });
     }
 
-    //@TODO Access-Control-Allow-Origin need redirect_uri
+    //@TODO Access-Control-Allow-Origin, need redirect_uri and need Request by server side
     function pocketAuthorization() {
-        $.ajax({
+        return $.ajax({
             type: 'get',
             url: pocketHost + "auth/authorize",
             data: {request_token: pocketCode, redirect_uri: appHost},
@@ -102,27 +121,39 @@ var BookmarksToPocket = function($) {
         $("#content").html(html);
     }
 
+    //@TODO Access-Control-Allow-Origin, need request by server side
     function pocketOAuthAuthorize() {
         console.log('call> BookmarksToPocket:pocketOAuthAuthorize');
-        $.ajax({
+        return $.ajax({
             type: 'post',
             dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
             url: pocketHost + 'v3/oauth/authorize',
-            data: {"consumer_key": pocketCustomerKey, "code": pocketCode},
+            data: {"consumer_key": pocketConsumerKey, "code": pocketCode},
             success: setAccessToken
         });
     }
-
-
-    //code=2d838e69-1d84-e209-73af-ae88b8
-    //access_token=df7cee47-1c24-6e83-3d46-2ce2bb&username=anderson.mota
-
-    init();
+    
+    function pocketAdd(bookmark) {
+        var data = bookmark;
+        $.ajax({
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
+            url: pocketHost + "v3/add",
+            data: $.extend(data, {"consumer_key": pocketConsumerKey, "access_token": pocketAccessToken}),
+            success: function(data) {
+                if (data.status) {
+                    bookmarksAdded.push(bookmark);
+                }
+            }
+        });
+    }
 
     return {
         handleAuthorize: function() {
             console.log('call> BookmarksToPocket:handleAuthorize');
             pocketOAuthAuthorize();
         }
-    }
+    };
 }(jQuery);
